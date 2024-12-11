73fa6ef65a969a64b29aa96ad3a36489b157d119
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-12-11 10:33:30 -0300

    Aula2_11-12-2024_10-33

---
+++ aulas/src/components/header/index.jsx
@@ -0,0 +1,17 @@
+import styles from './Header.module.css';
+
+function Header() {
+    return(
+        <header className={styles.header}>
+            <span>estevamjannuzzi4.artstation.com</span>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/sobre">Sobre</a>
+                <a href="/projetos">Projetos</a>
+                <a href="/contato">Contato</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
