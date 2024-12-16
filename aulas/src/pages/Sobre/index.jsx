25ce8866753356f1263dbd747ac9151aabcc61b0
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-12-16 20:11:04 -0300

    aula07_encaminhada

--- aulas/src/pages/Sobre/index.jsx
+++ aulas/src/pages/Sobre/index.jsx
@@ -1,6 +1,45 @@
+import styles from './Sobre.module.css'
+import avatar from './images/avatar.png'
+import html from './images/icon-html.svg'
+import css from './images/icon-css.svg'
+import js from './images/icon-js.svg'
+import react from './images/Group.png'
+import node from './images/icon-node.svg'
+import sql from './images/icon-sql.svg'
+
 function Sobre() {
     return (
-        <h2>Sobre</h2>
+        <section className={styles.sobre}>
+
+            <div className={styles.bio}>
+                <img src={avatar} alt="Avatar" className={styles.avatar} />
+                <div className={styles.textos}>
+                    <h2>Sobre</h2>
+
+                    <p>Sou Estevam Jannuzzi <br />
+                        3D Designer</p>
+
+                    <p>Trabalho com produção de projetos 3D, desde 2010.</p>
+
+                    <p>Sou apaixonado por transformar ideias em realidade digital.</p>
+
+                    <p>Especializado em criação e aplicações dinâmicas e intuitivas, <br />
+                        com foco na experiência do espectador.</p>
+                </div>
+            </div>
+
+            <div className={styles.techs}>
+                <h3>Techs</h3>
+                <div className={styles.icones}>
+                    <img src={html} alt="Ícone do HTML" />
+                    <img src={css} alt="Ícone do CSS" />
+                    <img src={js} alt="Ícone do JS" />
+                    <img src={react} alt="Ícone do React" />
+                    <img src={node} alt="Ícone do Node" />
+                    <img src={sql} alt="Ícone do SQL" />
+                </div>
+            </div>
+        </section>
     )
 }
 
