ed9f69846f3c70b80b0c983917b47524f05502fb
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2025-01-06 15:21:43 -0300

    aula09_atemetade

--- aulas/src/pages/Projetos/index.jsx
+++ aulas/src/pages/Projetos/index.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react"
 import Card from "../../components/Card"
+import styles from './Projetos.module.css'
 
 function Projetos() {
+
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        const buscarRepositorios = async () => {
+            const response = await fetch('https://api.github.com/users/edsonmaia/repos')
+            const data = await response.json()
+            setRepositories(data)
+        }
+        buscarRepositorios()
+    }, [])
+
     return (
-        <>
+        <section className={StyleSheet.projetos}>
             <h2>Projetos</h2>
-            <Card />
-            <Card />
-            <Card />
-        </>
+            <section className={styles.lista}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </section>
+        </section>
     )
 }
 
